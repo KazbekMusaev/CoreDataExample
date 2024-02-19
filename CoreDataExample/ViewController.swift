36b9e98a09909cb115d2daa0436a7d4ce59291bc
KazbekMusaev <145585208+KazbekMusaev@users.noreply.github.com>
2024-02-19 15:20:53 +0300

    Initial Commit

---
+++ CoreDataExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by apple on 19.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
